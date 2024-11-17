7dc3e87eac95056cde463e11024df729d464667a
Bruno Machado <126995337+Brunovalerio2018@users.noreply.github.com>
2024-11-17 17:09:39 -0300

    Update Perfil.tsx

--- src/screens/Perfil.tsx
+++ src/screens/Perfil.tsx
@@ -1,265 +1,191 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet, ScrollView, Alert, Button, Linking, Image, TouchableOpacity } from 'react-native';
-import { DataTable } from 'react-native-paper';
-import * as ImagePicker from 'expo-image-picker';
-import { BarChart } from 'react-native-chart-kit';
+import { View, Text, StyleSheet, ScrollView, Alert, Linking, Image, TouchableOpacity } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import api from '../app';
+import { Avatar } from 'react-native-paper';
 
-interface UserStatus {
-  nome: '';
-  email: '';
-  endereco: '';
-  cpf: '';
-  matricula: '';
+interface StatusDoUsuario {
+  nome: string;
+  email: string;
+  endereco: string;
+  cpf: string;
+  matricula: string;
+  notas: string[];
+  faltas: string[];
+  progressoAcademico: string;
+  curso: string;
+  professor: string;
 }
 
-const Perfil =  ({ navigation }: any) => {
-  const [userStatus, setUserStatus] = useState<UserStatus | null>(null);
-  const [image, setImage] = useState<string | null>(null);
-  const [loading, setLoading] = useState(true); // Adicionando estado de carregamento
+const Perfil = ({ navigation }: any) => {
+  const [statusDoUsuario, setStatusDoUsuario] = useState<StatusDoUsuario | null>(null);
+  const [imagem, setImagem] = useState<string | null>(null);
+  const [loading, setLoading] = useState(false);
+  const [tokenAcesso, setTokenAcesso] = useState('');
 
-  const token =   localStorage.getItem('userToken');
+  const token = AsyncStorage.getItem('userToken').then(res => setTokenAcesso(res ?? '-'));
 
-  const pickImage = async () => {
-    let result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.Images,
-      allowsEditing: true,
-      aspect: [1, 1],
-      quality: 1,
-    });
 
-    if (!result.canceled) {
-      setImage(result.assets[0].uri);
-    }
-  };
-
-  const openGoogleClassroom = () => {
-    Linking.openURL('https://classroom.google.com/');
-  };
-
-  const fetchUserInfo = async (tokenAcesso: string,idUser:number) => {
+  const buscarInformacoesDoUsuario = async (tokenAcesso: string, idUsuario: number) => {
     try {
-     /* const response = await fetch('http://usuarios/UsuariosController_findById', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${token}`,
-        },
-        body: JSON.stringify({ id: '' }),
-      });
-*/
-
-api.defaults.headers.common.Authorization = `Bearer ${tokenAcesso}`
-
-console.log(api.defaults.headers.common.Authorization)
-const response = await api.post(`usuarios/busca-um`,{id:idUser})
-      const data = await response.data;
-
-      if (data.id) {
-        console.log('Informações do usuário:', data);
-        setUserStatus({
-          nome: data.nome || '',
-          email: data.email || '',
-          endereco: data.endereco || '',
-          cpf: data.cpf || '',
-          matricula: data.matricula || ''
+      api.defaults.headers.common.Authorization = `Bearer ${tokenAcesso}`;
+      const resposta = await api.post('usuarios/busca-um', { id: idUsuario });
+      const dados = await resposta.data;
+      if (dados.id) {
+        setStatusDoUsuario({
+          nome: dados.nome || '',
+          email: dados.email || '',
+          endereco: dados.endereco || '',
+          cpf: dados.cpf || '',
+          matricula: dados.matricula || '',
+          notas: dados.notas || [],
+          faltas: dados.faltas || [],
+          progressoAcademico: dados.progressoAcademico || '',
+          curso: dados.curso || '',
+          professor: dados.professor || '',
         });
       } else {
-        console.log('Erro ao buscar informações do usuário:', data.message);
+        Alert.alert('Erro', 'Erro ao buscar informações do usuário.');
       }
-    } catch (error) {
-      console.error('Erro ao buscar informações do usuário:', error);
+    } catch (erro) {
+      console.error('Erro ao buscar informações do usuário:', erro);
       Alert.alert('Erro', 'Falha ao buscar os dados. Tente novamente mais tarde.');
     } finally {
-      setLoading(false); // Quando os dados forem carregados ou falharem
+      setLoading(false);
     }
   };
-
-  
 
   const handleLogout = async () => {
     try {
       await AsyncStorage.removeItem('userToken');
       navigation.replace('Login');
-    } catch (error) {
-      console.error('Erro ao fazer logout:', error);
+    } catch (erro) {
+      console.error('Erro ao fazer logout:', erro);
       Alert.alert('Erro', 'Falha ao sair. Tente novamente mais tarde.');
     }
   };
 
   useEffect(() => {
-    if (token) {
-      fetchUserInfo(token, 5);
+    if (tokenAcesso) {
+      buscarInformacoesDoUsuario(tokenAcesso, 5); // Exemplo com ID 5
     }
-  }, [token]);// Chama a função de buscar dados ao montar o componente
+  }, [tokenAcesso]);
+
   return (
     <ScrollView style={styles.container}>
-      <View style={styles.profileHeader}>
-        <TouchableOpacity onPress={pickImage}>
-          {image ? (
-            <Image source={{ uri: image }} style={styles.profileImage} />
-          ) : (
-            <View style={styles.profileImagePlaceholder}>
-              <Text style={styles.profileImageText}>Importar Foto</Text>
-            </View>
-          )}
-        </TouchableOpacity>
-        <Text style={styles.profileName}>{userStatus?.nome || 'Nome Não Disponível'}</Text>
+      <View style={styles.cabecalhoPerfil}>
+        <Text style={styles.nomePerfil}>{statusDoUsuario?.nome || 'Nome Não Disponível'}</Text>
+        <Text style={styles.detalhesPerfil}>Matrícula: {statusDoUsuario?.matricula || 'Não disponível'}</Text>
+        <Text style={styles.detalhesPerfil}>Curso: {statusDoUsuario?.curso || 'Não disponível'}</Text>
       </View>
 
-      <View style={styles.listItem}>
-        <Text style={styles.itemTitle}>Email:</Text>
-        <Text style={styles.itemText}>{userStatus?.email || 'Não disponível'}</Text>
-      </View>
-
-      <View style={styles.listItem}>
-        <Text style={styles.itemTitle}>Matrícula:</Text>
-        <Text style={styles.itemText}>{userStatus?.matricula || 'Não disponível'}</Text>
-      </View>
-
-      <View style={styles.sectionContainer}>
-        <Text style={styles.sectionTitle}>Dados Pessoais</Text>
-        <View style={styles.listItem}>
-          <Text style={styles.itemTitle}>Endereço:</Text>
-          <Text style={styles.itemText}>{userStatus?.endereco}</Text>
+      <View style={styles.secaoContainer}>
+        <Text style={styles.tituloSecao}>Dados Pessoais</Text>
+        <View style={styles.itemLista}>
+          <Text style={styles.tituloItem}>Email:</Text>
+          <Text style={styles.textoItem}>{statusDoUsuario?.email || 'Não disponível'}</Text>
+        </View>
+        <View style={styles.itemLista}>
+          <Text style={styles.tituloItem}>Endereço:</Text>
+          <Text style={styles.textoItem}>{statusDoUsuario?.endereco || 'Não disponível'}</Text>
         </View>
       </View>
 
-      <View style={styles.sectionContainer}>
-        <Text style={styles.sectionTitle}>Campos Acadêmicos</Text>
+      <View style={styles.secaoContainer}>
+        <Text style={styles.tituloSecao}>Acompanhamento Acadêmico</Text>
+        <View style={styles.itemLista}>
+          <Text style={styles.tituloItem}>Progresso Acadêmico:</Text>
+          <Text style={styles.textoItem}>{statusDoUsuario?.progressoAcademico || 'Não disponível'}</Text>
+        </View>
       </View>
 
-      
-      <TouchableOpacity style={styles.logoutButton} onPress={handleLogout}>
-        <Text style={styles.buttonText}>Sair da Conta</Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.button} onPress={openGoogleClassroom}>
-        <Text style={styles.buttonText}>Abrir Google Sala de Aula</Text>
-      </TouchableOpacity>
+      <View style={styles.secaoContainer}>
+        <Text style={styles.tituloSecao}>Informações de Comunicação</Text>
+        <TouchableOpacity style={styles.botao} onPress={() => Linking.openURL('https://classroom.google.com/')}>
+          <Text style={styles.textoBotao}>Abrir Google Sala de Aula</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.botao} onPress={() => Linking.openURL('https://docs.google.com/forms')}>
+          <Text style={styles.textoBotao}>Abrir Formulário Google</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.secaoContainer}>
+        <TouchableOpacity style={styles.botaoLogout} onPress={handleLogout}>
+          <Text style={styles.textoBotao}>Sair</Text>
+        </TouchableOpacity>
+      </View>
     </ScrollView>
   );
 };
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 2,
+    padding: 20,
     backgroundColor: '#f8f9fa',
   },
-  profileHeader: {
+  cabecalhoPerfil: {
     alignItems: 'center',
-    marginBottom: 10,
+    marginBottom: 20,
   },
-  profileImage: {
+  imagemPerfil: {
     width: 120,
     height: 120,
     borderRadius: 60,
     marginBottom: 10,
   },
-  profileImagePlaceholder: {
-    width: 120,
-    height: 120,
-    borderRadius: 90,
-    backgroundColor: '#e0e0e0',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  profileImageText: {
-    color: '#777',
-    fontSize: 14,
-  },
-  profileName: {
+  nomePerfil: {
     fontSize: 24,
     fontWeight: 'bold',
     color: '#333',
-    
   },
-  sectionContainer: {
+  detalhesPerfil: {
+    marginTop: 15,
+    fontSize: 16,
+    color: '#555',
+  },
+  secaoContainer: {
     marginBottom: 20,
   },
-  sectionTitle: {
+  tituloSecao: {
     fontSize: 18,
     fontWeight: 'bold',
     color: '#333',
     marginBottom: 10,
   },
-  listItem: {
+  itemLista: {
     flexDirection: 'row',
     justifyContent: 'space-between',
-    marginBottom: 10,
+    marginBottom: 15,
   },
-  itemTitle: {
+  tituloItem: {
     fontSize: 16,
     fontWeight: 'bold',
     color: '#333',
   },
-  itemText: {
+  textoItem: {
     fontSize: 16,
     color: '#555',
   },
-  chartContainer: {
-    marginBottom: 50,
-    alignItems: 'center',
-  },
-  button: {
+  botao: {
     backgroundColor: '#4CAF50',
-    padding: 10,
+    paddingVertical: 8,
+    paddingHorizontal: 16,
     borderRadius: 5,
     alignItems: 'center',
-    marginTop: 20,
+    marginTop: 15,
   },
-  buttonText: {
+  textoBotao: {
     color: '#fff',
-    fontSize: 16,
-    fontWeight: 'bold',
-    borderRadius: 20
-  },
-  calendarContainer: {
-    marginTop: 30,
-    alignItems: 'center',
-  },
-  calendarTitle: {
-    fontSize: 28,
-    fontWeight: 'bold',
-    color: '#333',
-    marginBottom: 10,
-  },
-  calendar: {
-    width: '100%',
-    padding: 0,
-    backgroundColor: '#fff',
-    borderRadius: 8,
-    shadowColor: '#000',
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.1,
-    shadowRadius: 10,
-  },
-  calendarDay: {
-    fontSize: 16,
-    color: '#333',
-  },
-  calendarWeekend: {
-    fontSize: 16,
-    color: '#e53935',
-  },
-  calendarEvent: {
-    fontSize: 16,
-    color: '#4CAF50',
-  },
-  footer: {
-    marginTop: 30,
-    alignItems: 'center',
-  },
-  footerText: {
     fontSize: 14,
-    color: '#777',
+    fontWeight: 'bold',
   },
-  logoutButton: {
+  botaoLogout: {
     backgroundColor: '#FF5722',
     padding: 10,
     borderRadius: 5,
     alignItems: 'center',
-    marginTop: 0,
+    marginTop: 20,
   },
 });
 
