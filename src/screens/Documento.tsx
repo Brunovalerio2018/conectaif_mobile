6096d1f2ebf7aa1a5f971853cee0d6b1d5c247b3
Bruno Machado <126995337+Brunovalerio2018@users.noreply.github.com>
2024-11-18 23:21:32 -0300

    Update Documento.tsx

--- src/screens/Documento.tsx
+++ src/screens/Documento.tsx
@@ -1,29 +1,54 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet , Text, View } from "react-native";
+// screens/Documentos.tsx
 
+import React from 'react';
+import { View, Text, Button, FlatList, StyleSheet, TextInput } from 'react-native';
 
-export default function Documentos(){
-    return(
-        <View style={styles.conteiner}>
-            <Text style={styles.title}>Login</Text>
-        </View>
-    );
-}
+const Documentos = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Documentos</Text>
+      <Button title="Upload de Documento" onPress={() => {/* Navegar para tela de upload */}} />
 
+      {/* Campo de busca */}
+      <TextInput style={styles.searchInput} placeholder="Buscar documentos..." />
+
+      {/* Lista de documentos */}
+      <FlatList
+        data={''}
+        keyExtractor={(item) => item.toString()}
+        renderItem={({ item }) => (
+          <View style={styles.documentItem}>
+            <Text>{item}</Text>
+            <Button title="Download" onPress={() => {/* Função de download */}} />
+          </View>
+        )}
+      />
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    conteiner:{
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center'
+  container: { 
+    flex: 1, 
+    padding: 20
+   },
+  title: { 
+    fontSize: 24, 
+    fontWeight: 'bold', 
+    marginBottom: 10 },
+  searchInput: { 
+    padding: 10, 
+    borderColor: '#ccc', 
+    borderWidth: 1, 
+    borderRadius: 8, 
+    marginBottom: 10 
+  },
+  documentItem: { 
+    marginVertical: 10, 
+    padding: 15, 
+    backgroundColor: '#f2f2f2', 
+    borderRadius: 8 
+  }
+});
 
-    },
-    title:{
-        fontSize: 22,
-        fontWeight: 'bold'
-    }
-})
-
-
+export default Documentos;
