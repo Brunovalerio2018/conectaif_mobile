119dde32d00d353f591ddb1069f6cdd36e6f7e9b
Bruno Machado <126995337+Brunovalerio2018@users.noreply.github.com>
2024-11-28 08:06:01 -0300

    Update tab.routes.tsx

--- src/routes/tab.routes.tsx
+++ src/routes/tab.routes.tsx
@@ -1,17 +1,15 @@
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import * as React from "react";
 import { Feather } from '@expo/vector-icons';
-import { Alert, Text, TouchableOpacity, StyleSheet, View, ActivityIndicator } from "react-native";
+import { Alert, Text, TouchableOpacity, StyleSheet, View, ActivityIndicator, Image } from "react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import Perfil from "../screens/Perfil";
-import Grupo from "../screens/Grupo";
 import Ocorrencias from "../screens/Ocorrencias";
 import Reunioes from "../screens/Reunioes";
 import Documentos from "../screens/Documento";
 import Notificacoes from "../screens/Notificacoes";
 import { useNavigation } from "@react-navigation/native";
 
-// Definindo as cores de forma centralizada
 const colors = {
   primaryGreen: '#359830',
   activeRed: '#7CFC00',
@@ -20,7 +18,6 @@ const colors = {
   lightGray: '#f2f2f2',
 };
 
-// Função auxiliar para criar os ícones
 const DrawerIcon = (name: string, color: string) => (
   <Feather name={name} size={24} color={color} />
 );
@@ -31,11 +28,9 @@ export default function DrawerRoutes() {
   const navigation = useNavigation();
   const [isLoggingOut, setIsLoggingOut] = React.useState(false);
 
-  // Função de logout com efeito de carregamento e temporizador
   const handleLogout = async () => {
     setIsLoggingOut(true);
     try {
-      // Simulando um tempo de espera com um temporizador de 2 segundos
       setTimeout(async () => {
         await AsyncStorage.removeItem('userToken');
         navigation.reset({
@@ -43,7 +38,7 @@ export default function DrawerRoutes() {
           routes: [{ name: 'Login' }],
         });
         setIsLoggingOut(false);
-      }, 5000); // Diminui o tempo de espera para 5 segundos
+      }, 5000);
     } catch (error) {
       console.error('Erro ao fazer logout:', error);
       Alert.alert('Erro', 'Falha ao sair. Tente novamente mais tarde.');
@@ -53,22 +48,20 @@ export default function DrawerRoutes() {
 
   return (
     <Drawer.Navigator
-    screenOptions={{
-      headerShown: true,
-      headerStyle: { backgroundColor: colors.primaryGreen },
-      headerTintColor: colors.white,
-      headerTitleStyle: { fontWeight: 'bold' },
-      drawerActiveTintColor: colors.activeRed,
-      drawerInactiveTintColor: colors.white,
-      drawerStyle: {
-        backgroundColor: colors.primaryGreen,
-        width: 240,
-        transform: [{ perspective: 800 }, { rotateY: '10deg' }],  // Efeito 3D
-      },
-      drawerItemStyle: { marginVertical: 8 }, // Espaço entre os itens do menu
-    }}
+      screenOptions={{
+        headerShown: true,
+        headerStyle: { backgroundColor: colors.primaryGreen },
+        headerTintColor: colors.white,
+        headerTitleStyle: { fontWeight: 'bold' },
+        drawerActiveTintColor: colors.activeRed,
+        drawerInactiveTintColor: colors.white,
+        drawerStyle: {
+          backgroundColor: colors.primaryGreen,
+          width: 240,
+        },
+        drawerItemStyle: { marginVertical: 8 },
+      }}
     >
-      {/* Seção Principal */}
       <Drawer.Screen
         name="Perfil"
         component={Perfil}
@@ -84,30 +77,12 @@ export default function DrawerRoutes() {
         }}
       />
       <Drawer.Screen
-        name="Reunioes"
+        name="Reuniões"
         component={Reunioes}
         options={{
           drawerIcon: ({ color }) => DrawerIcon("calendar", color),
         }}
       />
-      <Drawer.Screen
-        name="Grupo"
-        component={Grupo}
-        options={{
-          drawerIcon: ({ color }) => DrawerIcon("users", color),
-        }}
-      />
-      
-      {/* Separador entre seções principais e secundárias */}
-      <Drawer.Screen
-        name="Separador"
-        component={() => null}
-        options={{
-          drawerLabel: () => <View style={styles.separator} />,
-        }}
-      />
-
-      {/* Seção de Documentos e Notificações */}
       <Drawer.Screen
         name="Documentos"
         component={Documentos}
@@ -116,40 +91,47 @@ export default function DrawerRoutes() {
         }}
       />
       <Drawer.Screen
-        name="Notificacoes"
+        name="Notificaes"
         component={Notificacoes}
         options={{
           drawerIcon: ({ color }) => DrawerIcon("bell", color),
         }}
       />
-
-      {/* Separador antes do Logout */}
-      <Drawer.Screen
-        name="Separador2"
-        component={() => null}
-        options={{
-          drawerLabel: () => <View style={styles.separator} />,
-        }}
-      />
-
-      {/* Logout */}
       <Drawer.Screen
         name="Logout"
         component={() => null}
         options={{
           drawerLabel: () => (
-            <TouchableOpacity style={styles.logoutButton} onPress={handleLogout}>
+            <TouchableOpacity style={styles.logoutContainer} onPress={handleLogout}>
               {isLoggingOut ? (
                 <View style={styles.loadingContainer}>
                   <ActivityIndicator size="small" color={colors.white} />
                   <Text style={styles.loadingText}>Saindo...</Text>
                 </View>
               ) : (
-                <Text style={styles.buttonText}>Sair</Text>
+                <View style={styles.logoutContent}>
+                  <Feather name="log-out" size={24} color={colors.white} />
+                  <Text style={styles.logoutText}>Sair</Text>
+                </View>
               )}
             </TouchableOpacity>
           ),
-          drawerIcon: ({ color }) => DrawerIcon("log-out", color),
+          drawerIcon: () => null,
+        }}
+      />
+      <Drawer.Screen
+        name="LogoFooter"
+        component={() => null}
+        options={{
+          drawerLabel: () => (
+            <View style={styles.footerContainer}>
+              <Image
+                source={require('../../assets/logoConectaIF_branco.png')}
+                style={styles.logo}
+                resizeMode="contain"
+              />
+            </View>
+          ),
         }}
       />
     </Drawer.Navigator>
@@ -157,25 +139,45 @@ export default function DrawerRoutes() {
 }
 
 const styles = StyleSheet.create({
-  logoutButton: {
-    padding: 10,
+  logoutContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingVertical: 15,
+    paddingHorizontal: 10,
+    backgroundColor: colors.primaryGreen,
+    borderRadius: 8,
+    marginHorizontal: 10,
   },
-  buttonText: {
+  logoutContent: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  logoutText: {
+    marginLeft: 10,
     fontSize: 16,
     color: colors.white,
+    fontWeight: 'bold',
+  },
+  logoutLogo: {
+    marginLeft: 10,
+    width: 24,
+    height: 24,
   },
   loadingContainer: {
     flexDirection: 'row',
     alignItems: 'center',
   },
   loadingText: {
-    marginLeft: 10,
-    color: colors.white,
+    marginLeft: 20,
+    color: colors.primaryGreen,
     fontSize: 14,
   },
-  separator: {
-    height: 1,
-    backgroundColor: colors.lightGray,
-    marginVertical: 16,
+  footerContainer: {
+    alignItems: 'center',
+    marginTop: 20,
+  },
+  logo: {
+    width: 220,
+    height: 650,
   },
 });
