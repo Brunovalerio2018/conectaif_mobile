af89c2e04d73ee8634fe485f592fa3a39fc91895
Bruno Machado <126995337+Brunovalerio2018@users.noreply.github.com>
2024-11-28 08:03:25 -0300

    Update Login.tsx

--- src/screens/Principal/Login.tsx
+++ src/screens/Principal/Login.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { View, Text, TextInput, TouchableOpacity, Image, Animated, StyleSheet, Alert } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons'; // Importação para ícones de visibilidade
 import { useNavigation } from '@react-navigation/native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
@@ -10,12 +11,14 @@ const LoginHome = () => {
   const [senha, setSenha] = useState('');
   const [mostrarSenha, setMostrarSenha] = useState(false);
   const [salvarUsuario, setSalvarUsuario] = useState(false);
-  const [logoAnim, setLogoAnim] = useState(new Animated.Value(-100));  // Inicia a logo fora da tela
-  const opacityAnim = useRef(new Animated.Value(1)).current;
+  const [logoAnim, setLogoAnim] = useState(new Animated.Value(-100));
   const navigation = useNavigation();
 
+  const toggleMostrarSenha = () => {
+    setMostrarSenha(!mostrarSenha);
+  };
+
   useEffect(() => {
-    // Anima logo de cima para baixo ao carregar ou ao fazer logout
     Animated.spring(logoAnim, {
       toValue: 0,
       friction: 5,
@@ -25,33 +28,22 @@ const LoginHome = () => {
     const checkLogin = async () => {
       const tokenData = await AsyncStorage.getItem('userToken');
       if (tokenData) {
-        // Se já houver um token, redireciona para a tela principal
         navigation.navigate("TabRoutes");
       }
 
-      // Busca o nome de usuário salvo e a senha
       const usuarioSalvo = await AsyncStorage.getItem('savedUsername');
       const senhaSalva = await AsyncStorage.getItem('savedPassword');
       if (usuarioSalvo && senhaSalva) {
         setUsuario(usuarioSalvo);
         setSenha(senhaSalva);
-        setSalvarUsuario(true); // Marca o checkbox se houver dados salvos
+        setSalvarUsuario(true);
       }
     };
     checkLogin();
   }, [navigation]);
 
-  const toggleMostrarSenha = () => {
-    setMostrarSenha(!mostrarSenha);
-  };
-
-  const toggleSalvarUsuario = () => {
-    setSalvarUsuario(!salvarUsuario);
-  };
-
   const handlePress = async () => {
     if (!senha) {
-      console.log("Digite sua senha");
       Alert.alert("Erro de Login", "Digite sua senha");
       return;
     }
@@ -59,42 +51,23 @@ const LoginHome = () => {
     try {
       const retorno = await api.post('autorizacao/login', { login: usuario, senha: senha });
       const token = retorno?.data?.access_token;
+      console.log(retorno?.data)
 
       if (token) {
         await AsyncStorage.setItem('userToken', token);
-
         if (salvarUsuario) {
-          // Salva o nome de usuário e a senha no AsyncStorage
           await AsyncStorage.setItem('savedUsername', usuario);
           await AsyncStorage.setItem('savedPassword', senha);
         } else {
-          // Remove o nome de usuário e senha do AsyncStorage
           await AsyncStorage.removeItem('savedUsername');
           await AsyncStorage.removeItem('savedPassword');
         }
 
-        Animated.sequence([
-          Animated.timing(opacityAnim, {
-            toValue: 0.,
-            duration: 100,
-            useNativeDriver: true,
-          }),
-          Animated.timing(opacityAnim, {
-            toValue: 1,
-            duration: 100,
-            useNativeDriver: true,
-          }),
-        ]).start();
-
-        console.log("Carregando seu perfil");
-        Alert.alert("Login bem-sucedido!", "Bem-vindo ao sistema.");
         navigation.navigate("TabRoutes");
       } else {
-        console.log("Sua senha está errada");
         Alert.alert("Erro de Login", "Sua senha está errada");
       }
     } catch (error) {
-      console.error("Erro ao realizar login: ", error);
       Alert.alert("Erro de Login", "Houve um problema ao tentar fazer login. Tente novamente.");
     }
   };
@@ -118,54 +91,42 @@ const LoginHome = () => {
       <Text style={styles.label1}>Usuário:</Text>
       <TextInput
         style={styles.input}
-        placeholder="  Nome de usuário"
+        placeholder="    Digite seu usuário"
         value={usuario}
         onChangeText={setUsuario}
       />
 
       <Text style={styles.label2}>Senha:</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="   Digite sua senha"
-        secureTextEntry={!mostrarSenha}
-        value={senha}
-        onChangeText={setSenha}
-      />
-
-      <View style={styles.optionsContainer}>
-        <TouchableOpacity onPress={toggleMostrarSenha}>
-          <Text style={styles.optionText}>
-            {mostrarSenha ? 'Ocultar a senha' : 'Exibir a senha'}
-          </Text>
-        </TouchableOpacity>
-
-        <View style={styles.spaceBetweenOptions} />
-
-        <TouchableOpacity onPress={handleRecuperarSenha}>
-          <Text style={styles.optionText}>
-            Esqueceu ou deseja alterar sua senha?
-          </Text>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.inputSenha}
+          placeholder="   Digite sua senha"
+          secureTextEntry={!mostrarSenha}
+          value={senha}
+          onChangeText={setSenha}
+        />
+        <TouchableOpacity onPress={toggleMostrarSenha} style={styles.eyeIcon}>
+          <Icon 
+            name={mostrarSenha ? 'visibility' : 'visibility-off'} 
+            size={24} 
+            color="#359830"
+          />
         </TouchableOpacity>
       </View>
 
-      {/* Checkbox para salvar o nome de usuário e a senha */}
-      <View style={styles.checkboxContainer}>
-        <TouchableOpacity onPress={toggleSalvarUsuario}>
-          <Text style={styles.checkboxLabel}>
-            {salvarUsuario ? '☑' : '☐'} Salvar nome de usuário e senha
-          </Text>
-        </TouchableOpacity>
-      </View>
+      {/* Adicionado o botão para recuperação de senha */}
+      <TouchableOpacity onPress={handleRecuperarSenha}>
+        <Text style={styles.optionText}>
+          Esqueceu ou deseja alterar sua senha?
+        </Text>
+      </TouchableOpacity>
 
-      <Animated.View style={{ opacity: opacityAnim }}>
-        <TouchableOpacity
-          style={[styles.neonButton, { transform: [{ scale: 1.1 }] }]} // Efeito de escala no toque
-          onPress={handlePress}
-          activeOpacity={0.8}
-        >
-          <Text style={styles.buttonText}>Acessar</Text>
-        </TouchableOpacity>
-      </Animated.View>
+      <TouchableOpacity
+        style={[styles.neonButton]}
+        onPress={handlePress}
+      >
+        <Text style={styles.buttonText}>Acessar</Text>
+      </TouchableOpacity>
 
       <Text style={styles.watermark}>SISTEMA UNIFICADO CONECTAIF</Text>
     </View>
@@ -181,35 +142,37 @@ const styles = StyleSheet.create({
   },
   logoContainer: {
     alignItems: 'center',
-    marginBottom: 0,
+    marginBottom: -30,
   },
   logo: {
     width: 1000,
-    height: 550,
+    height: 450,
+    marginVertical: -40
   },
   title: {
-    fontSize: 25,
+    fontSize: 18,
     marginBottom: 10,
     textAlign: 'center',
     color: '#000',
   },
   label1: {
-    fontSize: 20,
+    fontSize: 18,
     marginBottom: 0,
     marginLeft: -220,
     color: '#333',
   },
   label2: {
-    fontSize: 20,
+    fontSize: 18,
     marginBottom: 0,
     borderTopLeftRadius: 20,
     marginLeft: -210,
   },
   input: {
-    borderWidth: 1,
-    borderColor: '#ccc',
-    padding: 3,
-    marginBottom: 13,
+    borderWidth: 2,
+    borderColor: '#359830',
+    padding: 4,
+    paddingLeft: 12, // Adicionando espaço para o cursor
+    marginBottom: 15,
     borderRadius: 20,
     backgroundColor: '#eaeaea',
     width: '100%',
@@ -224,37 +187,39 @@ const styles = StyleSheet.create({
   },
   optionText: {
     color: '#0066cc',
-    marginBottom: -10,
+    marginBottom: 10,
+    textDecorationLine: 'underline',
+    marginVertical: 20
   },
   checkboxContainer: {
-    marginVertical: 30,
+    marginVertical: 10,
   },
   checkboxLabel: {
     color: '#333',
-    fontSize: 16,
+    fontSize: 14,
   },
   neonButton: {
-    backgroundColor: '#359830',  // A cor #359830 foi mantida
-    paddingVertical: 12,
+    backgroundColor: '#359830',
+    paddingVertical: 2,
     paddingHorizontal: 20,
-    borderRadius: 50,
-    shadowRadius: 10,
+    borderRadius: 30,
+    shadowRadius: 50,
     elevation: 5,
     justifyContent: 'center',
-    marginVertical: 0,
+    marginVertical: -80,
     borderWidth: 0,
-    borderColor: '#359830',  // A cor #359830 foi mantida
+    borderColor: '#359830',
     alignItems: 'center',
     transform: [{ scale: 1 }],
-    transition: '0.3s',  // Transição para efeitos suaves
+  
   },
   buttonText: {
-    fontSize: 16,
+    fontSize: 17,
     color: '#fff',
     fontWeight: 'bold',
     textAlign: 'center',
-    textTransform: 'uppercase', // Deixa o texto em maiúsculas
-    letterSpacing: 2, // Espaçamento nas letras
+
+    letterSpacing: 5,
   },
   watermark: {
     position: 'absolute',
@@ -263,6 +228,30 @@ const styles = StyleSheet.create({
     fontSize: 12,
     color: 'rgba(0, 0, 0, 0.2)',
   },
+  inputContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    borderWidth: 2,
+    borderColor: '#359830',
+    borderRadius: 20,
+    backgroundColor: '#eaeaea',
+    paddingHorizontal: 10,
+    marginBottom: 75,
+    width: '100%',
+    maxWidth: 290,
+  },
+  inputSenha: {
+    flex: 1,
+    paddingVertical: 5,
+  },
+  eyeIcon: {
+    marginLeft: 10,
+  },
 });
 
 export default LoginHome;
+
+
+
+
+
