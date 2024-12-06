a5e07d464b19e073abb65b7c706a580b4859411a
Bruno Machado <126995337+Brunovalerio2018@users.noreply.github.com>
2024-12-06 00:51:16 -0300

    Update Login.tsx

--- src/screens/Principal/Login.tsx
+++ src/screens/Principal/Login.tsx
@@ -13,6 +13,8 @@ const LoginHome = () => {
   const [mostrarSenha, setMostrarSenha] = useState(false);
   const [salvarUsuario, setSalvarUsuario] = useState(false);
   const [logoAnim, setLogoAnim] = useState(new Animated.Value(-100));
+  const [loading, setLoading] = useState(false); // Estado de carregamento
+  const [progress, setProgress] = useState(new Animated.Value(0)); // Estado da barra de progresso
   const navigation = useNavigation();
 
   const toggleMostrarSenha = () => {
@@ -49,27 +51,43 @@ const LoginHome = () => {
       return;
     }
 
-    try {
-      const retorno = await api.post('autorizacao/login', { login: usuario, senha: senha });
-      const {access_token,...dadosUsuario} = retorno?.data;
-      
+    // Inicia o carregamento
+    setLoading(true);
 
-      if (access_token) {
-        await AsyncStorage.setItem('userToken', access_token);
-        await AsyncStorage.setItem('dadosUsuario',JSON.stringify( dadosUsuario))
-        if (salvarUsuario) {
-          await AsyncStorage.setItem('savedUsername', usuario);
-          await AsyncStorage.setItem('savedPassword', senha);
+    // Anima a barra de progresso
+    Animated.timing(progress, {
+      toValue: 1,
+      duration: 5000, // 5 segundos
+      useNativeDriver: false,
+    }).start();
+
+    try {
+      // Simula o delay do carregamento
+      setTimeout(async () => {
+        const retorno = await api.post('autorizacao/login', { login: usuario, senha: senha });
+        const { access_token, ...dadosUsuario } = retorno?.data;
+
+        if (access_token) {
+          await AsyncStorage.setItem('userToken', access_token);
+          await AsyncStorage.setItem('dadosUsuario', JSON.stringify(dadosUsuario));
+          if (salvarUsuario) {
+            await AsyncStorage.setItem('savedUsername', usuario);
+            await AsyncStorage.setItem('savedPassword', senha);
+          } else {
+            await AsyncStorage.removeItem('savedUsername');
+            await AsyncStorage.removeItem('savedPassword');
+          }
+
+          navigation.navigate("TabRoutes");
         } else {
-          await AsyncStorage.removeItem('savedUsername');
-          await AsyncStorage.removeItem('savedPassword');
+          Alert.alert("Erro de Login", "Sua senha está errada");
         }
 
-        navigation.navigate("TabRoutes");
-      } else {
-        Alert.alert("Erro de Login", "Sua senha está errada");
-      }
+        // Após 5 segundos, termina o carregamento
+        setLoading(false);
+      }, 5000);
     } catch (error) {
+      setLoading(false);
       Alert.alert("Erro de Login", "Houve um problema ao tentar fazer login. Tente novamente.");
     }
   };
@@ -133,11 +151,29 @@ const LoginHome = () => {
         </Text>
       </TouchableOpacity>
 
+      {/* Barra de progresso de carregamento */}
+      {loading && (
+        <View style={styles.loadingContainer}>
+          <Animated.View
+            style={[
+              styles.progressBar,
+              { width: progress.interpolate({ inputRange: [0, 1], outputRange: ['0%', '100%'] }) },
+            ]}
+          />
+        </View>
+      )}
+
+      {/* Botão de login com spinner de carregamento */}
       <TouchableOpacity
         style={[styles.neonButton]}
         onPress={handlePress}
+        disabled={loading} // Desabilita o botão durante o carregamento
       >
-        <Text style={styles.buttonText}>Acessar</Text>
+        {loading ? (
+          <Text style={styles.buttonText}>Carregando...</Text>
+        ) : (
+          <Text style={styles.buttonText}>Acessar</Text>
+        )}
       </TouchableOpacity>
 
       <Text style={styles.watermark}>SISTEMA UNIFICADO CONECTAIF</Text>
@@ -182,7 +218,7 @@ const styles = StyleSheet.create({
     borderWidth: 2,
     borderColor: '#359830',
     padding: 4,
-    paddingLeft: 12, // Adicionando espaço para o cursor
+    paddingLeft: 12,
     marginBottom: 15,
     borderRadius: 20,
     backgroundColor: '#eaeaea',
@@ -200,13 +236,13 @@ const styles = StyleSheet.create({
     color: '#0066cc',
     marginBottom: 10,
     textDecorationLine: 'underline',
-    marginVertical: 20
+    marginVertical: 20,
   },
   checkboxContainer: {
     flexDirection: 'row',
     alignItems: 'center',
     marginBottom: 55,
-    marginTop: -39
+    marginTop: -39,
   },
   checkboxLabel: {
     marginLeft: 8,
@@ -215,31 +251,28 @@ const styles = StyleSheet.create({
   },
   neonButton: {
     backgroundColor: '#359830',
-    paddingVertical: 2,
+    paddingVertical: 1,
     paddingHorizontal: 20,
     borderRadius: 30,
-    shadowRadius: 50,
+    shadowRadius: 1,
     elevation: 5,
     justifyContent: 'center',
     marginVertical: -80,
-    borderWidth: 0,
     borderColor: '#359830',
     alignItems: 'center',
     transform: [{ scale: 1 }],
-  
   },
   buttonText: {
     fontSize: 17,
     color: '#fff',
     fontWeight: 'bold',
     textAlign: 'center',
-
     letterSpacing: 5,
   },
   watermark: {
     position: 'absolute',
-    top: 30,
-    right: 10,
+    top: 65,
+    right: 20,
     fontSize: 12,
     color: 'rgba(0, 0, 0, 0.2)',
   },
@@ -262,7 +295,24 @@ const styles = StyleSheet.create({
   eyeIcon: {
     marginLeft: 10,
   },
-
+  loadingContainer: {
+    width: '80%',
+    height: 0,
+    backgroundColor: '#eaeaea',
+    borderRadius: 20,
+    marginVertical: 0,
+    
+  },
+  progressBar: {
+    height: '100%',
+    backgroundColor: '#359830',
+    borderRadius: 10,
+  },
 });
 
 export default LoginHome;
+
+
+
+
+
