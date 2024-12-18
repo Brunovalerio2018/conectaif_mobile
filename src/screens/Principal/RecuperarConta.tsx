e01023334b8c3dddcb6d296adae24aa91c681e41
Bruno Machado <126995337+Brunovalerio2018@users.noreply.github.com>
2024-12-18 16:12:36 -0300

    Update RecuperarConta.tsx

--- src/screens/Principal/RecuperarConta.tsx
+++ src/screens/Principal/RecuperarConta.tsx
@@ -1,282 +1,194 @@
-import React, { useState, useEffect } from "react";
-import {
-  StyleSheet,
-  Text,
-  View,
-  FlatList,
-  TextInput,
-  Modal,
-  Alert,
-  ActivityIndicator,
-  TouchableOpacity,
-  Animated,
-} from "react-native";
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import * as FileSystem from "expo-file-system"; // Importa o FileSystem para gerenciar downloads
-import api from "../app"; // Aqui você importa o axios ou qualquer instância de API configurada
+import React, { useState } from 'react';
+import { View, Text, TextInput, StyleSheet, Modal, TouchableOpacity, Image, Animated, Alert } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import api from '../../app'; // Certifique-se de que esse caminho está correto.
 
-interface Anexo {
-  id: string;
-  titulo: string;
-  arquivo: string;
-  tipo: string;
-}
-
-interface NotificaUser {
-  id: string;
-  titulo: string;
-  descricao: string;
-  idnotificacao: string;
-}
-
-const Notificacao = () => {
-  const [notificacoes, setNotificacoes] = useState<NotificaUser[]>([]);
-  const [searchQuery, setSearchQuery] = useState("");
-  const [tokenAcesso, setTokenAcesso] = useState<string | null>(null);
+const RecuperarConta = () => {
+  const [email, setEmail] = useState('');
   const [modalVisible, setModalVisible] = useState(false);
-  const [detalhesNotificacao, setDetalhesNotificacao] = useState<NotificaUser | null>(null);
-  const [anexos, setAnexos] = useState<Anexo[]>([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const scaleAnim = useState(new Animated.Value(0))[0];
+  const logoAnim = new Animated.Value(0);
+  const navigation = useNavigation();
 
-  useEffect(() => {
-    const getToken = async () => {
-      const storedToken = await AsyncStorage.getItem("userToken");
-      setTokenAcesso(storedToken);
-    };
-    getToken();
-  }, []);
-
-  useEffect(() => {
-    if (tokenAcesso) {
-      fetchNotificacoes(tokenAcesso);
+  const handleRecuperarConta = async () => {
+    if (!email.trim()) {
+      Alert.alert('Por favor, insira seu e-mail.');
+      return;
     }
-  }, [tokenAcesso]);
 
-  const fetchNotificacoes = async (token: string) => {
-    setIsLoading(true);
     try {
-      api.defaults.headers.common.Authorization = `Bearer ${token}`;
-      const response = await api.get("/notificacao");
-      setNotificacoes(response.data || []);
+      // Simula o delay do carregamento e envia a requisição POST para a API
+      setTimeout(async () => {
+        const response = await api.post('/email', {
+          destinatario: email,
+          assunto: 'Recuperação de Conta',
+          conteudo: 'Segue o link para recuperação de sua conta.',
+          anexos: ''
+        });
+
+        if (response?.status === 201) {
+          Alert.alert('E-mail enviado com sucesso!', 'Por favor, verifique sua caixa de entrada.');
+          setModalVisible(false); // Fecha o modal
+        } else {
+          throw new Error('Falha ao enviar o e-mail.');
+        }
+      }, 1000); // Simula um delay de 1 segundo
     } catch (error) {
-      Alert.alert("Erro", "Não foi possível carregar as notificações.");
-    } finally {
-      setIsLoading(false);
+      console.error(error);
+      Alert.alert('Erro ao enviar o e-mail.', 'Tente novamente mais tarde.');
     }
   };
 
-  const fetchAnexos = async (idNotificacao: string) => {
-    setIsLoading(true);
-    try {
-      const response = await api.get(`documento/anexos-notificacao/${idNotificacao}`);
-      setAnexos(response.data || []);
-    } catch (error) {
-      Alert.alert("Erro", "Não foi possível carregar os anexos.");
-    } finally {
-      setIsLoading(false);
-    }
-  };
-
-  const openModal = async (notificacao: NotificaUser) => {
-    setDetalhesNotificacao(notificacao);
-    setModalVisible(true);
-    Animated.spring(scaleAnim, {
+  const startLogoAnimation = () => {
+    Animated.timing(logoAnim, {
       toValue: 1,
+      duration: 1000,
       useNativeDriver: true,
-      friction: 5,
     }).start();
-    await fetchAnexos(notificacao.id);
   };
-
-  const closeModal = () => {
-    Animated.timing(scaleAnim, {
-      toValue: 0,
-      duration: 200,
-      useNativeDriver: true,
-    }).start(() => {
-      setModalVisible(false);
-      setDetalhesNotificacao(null);
-      setAnexos([]);
-    });
-  };
-
-  const downloadFile = async (url: string, filename: string) => {
-    const fileUri = `${FileSystem.documentDirectory}${filename}`;
-    try {
-      const { uri } = await FileSystem.downloadAsync(url, fileUri);
-      Alert.alert("Sucesso", `Arquivo salvo em: ${uri}`);
-    } catch (error) {
-      Alert.alert("Erro", "Não foi possível baixar o arquivo.");
-    }
-  };
-
-  const renderNotificacao = ({ item }: { item: NotificaUser }) => (
-    <TouchableOpacity style={styles.notificacaoItem} onPress={() => openModal(item)}>
-      <Text style={styles.notificacaoTitulo}>{item.titulo}</Text>
-      <Text style={styles.notificacaoDescricao}>{item.descricao}</Text>
-    </TouchableOpacity>
-  );
 
   return (
     <View style={styles.container}>
-      <TextInput
-        style={styles.searchInput}
-        placeholder="Buscar notificações..."
-        value={searchQuery}
-        onChangeText={setSearchQuery}
-      />
-      {isLoading ? (
-        <ActivityIndicator size="large" color="#6200EE" style={styles.loader} />
-      ) : (
-        <FlatList
-          data={notificacoes.filter((notificacao) =>
-            notificacao.titulo.toLowerCase().includes(searchQuery.toLowerCase())
-          )}
-          keyExtractor={(item) => item.idnotificacao}
-          renderItem={renderNotificacao}
-          ListEmptyComponent={<Text style={styles.noNotificacoes}>Nenhuma notificação encontrada</Text>}
+      <Animated.View style={[styles.logoContainer, { transform: [{ translateY: logoAnim }] }]}>
+        <Image
+          source={require('../../../assets/logoConectaIF.png')}
+          style={styles.logo}
+          resizeMode="contain"
         />
-      )}
+      </Animated.View>
 
-      {modalVisible && (
-        <Modal animationType="fade" transparent={true} visible={modalVisible} onRequestClose={closeModal}>
-          <View style={styles.modalContainer}>
-            <Animated.View style={[styles.modalContent, { transform: [{ scale: scaleAnim }] }]}>
-              {detalhesNotificacao && (
-                <>
-                  <Text style={styles.modalTitulo}>{detalhesNotificacao.titulo}</Text>
-                  <Text style={styles.modalDescricao}>{detalhesNotificacao.descricao}</Text>
-                  {anexos.length > 0 && (
-                    <>
-                      <Text style={styles.modalAnexosTitulo}>Anexos para download:</Text>
-                      <FlatList
-                        data={anexos}
-                        keyExtractor={(item) => item.id}
-                        renderItem={({ item }) => (
-                          <View style={styles.anexoItem}>
-                            <Text style={styles.anexoTitulo}>{item.titulo}</Text>
-                            <TouchableOpacity
-                              style={styles.downloadButton}
-                              onPress={() => downloadFile(item.arquivo, `${item.titulo}.${item.tipo}`)}
-                            >
-                              <Text style={styles.downloadButtonText}>Baixar</Text>
-                            </TouchableOpacity>
-                          </View>
-                        )}
-                      />
-                    </>
-                  )}
-                  <TouchableOpacity style={styles.closeButton} onPress={closeModal}>
-                    <Text style={styles.closeButtonText}>Fechar</Text>
-                  </TouchableOpacity>
-                </>
-              )}
-            </Animated.View>
+      <View style={styles.actionContainer}>
+        <TouchableOpacity style={styles.backButton} onPress={() => navigation.goBack()}>
+          <Text style={styles.backButtonText}>Voltar</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={() => { setModalVisible(true); startLogoAnimation(); }}>
+          <Text style={styles.recuperarContaText}>Recuperar conta...</Text>
+        </TouchableOpacity>
+      </View>
+
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => setModalVisible(false)}
+      >
+        <View style={styles.modalContainer}>
+          <View style={styles.modalContent}>
+            <Text style={styles.modalTitle}>Recuperar Conta</Text>
+
+            <TextInput
+              style={styles.input}
+              placeholder="Digite seu email"
+              value={email}
+              onChangeText={setEmail}
+              keyboardType="email-address"
+            />
+
+            <View style={styles.buttonContainer}>
+              <TouchableOpacity style={[styles.button, styles.sendButton]} onPress={handleRecuperarConta}>
+                <Text style={styles.buttonText}>Enviar</Text>
+              </TouchableOpacity>
+              <TouchableOpacity style={[styles.button, styles.cancelButton]} onPress={() => setModalVisible(false)}>
+                <Text style={styles.buttonText}>Cancelar</Text>
+              </TouchableOpacity>
+            </View>
           </View>
-        </Modal>
-      )}
+        </View>
+      </Modal>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  container: { flex: 1, padding: 16, backgroundColor: "#F5F5F5" },
-  searchInput: {
-    height: 40,
-    borderColor: "#6200EE",
-    borderWidth: 2,
-    borderRadius: 8,
-    marginBottom: 20,
-    paddingHorizontal: 12,
-    backgroundColor: "#FFF",
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#ffffff',
   },
-  notificacaoItem: {
-    backgroundColor: "#DFFFD6",
-    padding: 15,
-    marginVertical: 8,
-    borderRadius: 8,
+  actionContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: -80,
   },
-  notificacaoTitulo: { 
-    fontSize: 18, 
-    fontWeight: "bold", 
-    color: "#0D47A1" 
+  backButton: {
+    marginRight: 15,
+    paddingVertical: 5,
+    paddingHorizontal: 10,
+    backgroundColor: '#359830',
+    borderRadius: 18,
   },
-  notificacaoDescricao: { 
-    fontSize: 14, 
-    color: "#424242" 
+  backButtonText: {
+    color: '#fff',
+    fontWeight: 'bold',
   },
-  noNotificacoes: { 
-    textAlign: "center", 
-    marginTop: 20, 
-    fontSize: 16, 
-    color: "#BDBDBD" 
+  recuperarContaText: {
+    color: 'blue',
+    textDecorationLine: 'underline',
   },
   modalContainer: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "rgba(0, 0, 0, 0.5)",
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
   },
   modalContent: {
-    width: "85%",
-    backgroundColor: "#FFF",
+    width: '80%',
+    backgroundColor: 'white',
+    borderRadius: 15,
     padding: 20,
-    borderRadius: 12,
+    alignItems: 'center',
   },
-  modalTitulo: { 
-    fontSize: 22, 
-    fontWeight: "bold", 
-    color: "#359830", 
-    marginBottom: 10 
+  logoContainer: {
+    alignItems: 'center',
+    marginBottom: 20,
   },
-  modalDescricao: { 
-    fontSize: 16, 
-    color: "#616161", 
-    marginBottom: 10 
+  logo: {
+    width: 550,
+    height: 850,
   },
-  modalAnexosTitulo: { 
-    fontSize: 18, 
-    fontWeight: "bold", 
-    marginTop: 10, 
-    color: "#6200EE" 
+  modalTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 35,
+    textAlign: 'center',
+    color: '#000',
   },
-  anexoItem: { 
-    padding: 10, 
-    backgroundColor: "#EFEFEF", 
-    borderRadius: 6, 
-    marginVertical: 5 
+  input: {
+    height: 40,
+    borderWidth: 2,
+    borderColor: '#359830',
+    borderRadius: 20,
+    backgroundColor: '#eaeaea',
+    width: '100%',
+    paddingHorizontal: 10,
+    marginBottom: 20,
   },
-  anexoTitulo: { 
-    fontWeight: "bold", 
-    color: "#333" 
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '100%',
+    marginTop: 10,
   },
-  downloadButton: { 
-    marginTop: 5, 
-    backgroundColor: "#359830", 
-    padding: 10, 
-    borderRadius: 8 
+  button: {
+    width: '45%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 10,
+    borderRadius: 18,
+    marginHorizontal: 5,
   },
-  downloadButtonText: { 
-    color: "#FFF", 
-    fontWeight: "bold", 
-    textAlign: "center" 
+  sendButton: {
+    backgroundColor: '#359830',
   },
-  closeButton: { 
-    marginTop: 20, 
-    backgroundColor: "#359830", 
-    padding: 10, 
-    borderRadius: 8 
+  cancelButton: {
+    backgroundColor: 'red',
   },
-  closeButtonText: { 
-    color: "#FFF", 
-    fontWeight: "bold", 
-    textAlign: "center" 
-  },
-  loader: { 
-    flex: 1, 
-    justifyContent: "center" 
+  buttonText: {
+    color: '#fff',
+    fontWeight: 'bold',
   },
 });
 
-export default Notificacao;
+export default RecuperarConta;
